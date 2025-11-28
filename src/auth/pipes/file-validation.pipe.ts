d6a6212fed195f9bbea0439a5afcd0260b3defde
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 12:07:03 +0545

    feat:custome file validation pipe setup

---
+++ src/auth/pipes/file-validation.pipe.ts
@@ -0,0 +1,36 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform, Scope } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable({ scope: Scope.REQUEST })
+export class FileValidationPipe implements PipeTransform {
+    constructor(
+        private readonly configService: ConfigService
+    ) { }
+
+    transform(file: Express.Multer.File, metadata: ArgumentMetadata) {
+        const errors: string[] = [];
+        if (!file) {
+            return file
+        }
+        const fileUploadConfig = this.configService.get("fileupload")
+
+        const allowedMimeTypes: string[] = fileUploadConfig.allowedMimeTypes
+        const maxFileSize: number = fileUploadConfig.maxSize
+
+        if (!allowedMimeTypes.includes(file.mimetype)) {
+            errors.push(`Invalid File Type. Allowed ${allowedMimeTypes.join(', ')}`)
+        }
+        if (file.size > maxFileSize) {
+            errors.push(`File size limit exceeded. Max Allowed Limit: ${maxFileSize / (1024 * 1024)}MB`)
+        }
+        // If any errors → throw exception
+        if (errors.length > 0) {
+            throw new BadRequestException({
+                message: 'File validation failed',
+                errors,
+            });
+        }
+
+        return file
+    }
+}
