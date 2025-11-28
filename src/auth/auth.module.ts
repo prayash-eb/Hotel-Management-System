05a3b9c52916907fa3c6fba7b0ec6a2d8a5bb0e1
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 16:43:05 +0545

    Jwt passport strategy added in providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,21 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { FileValidationPipe } from './pipes/file-validation.pipe';
 import { CloudinaryService } from './cloudinary.service';
+import { JwtAccessStrategy, JwtRefreshStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule, JwtModule],
+  imports: [
+    UserModule, 
+    PassportModule, 
+    JwtModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService, CloudinaryService, FileValidationPipe],
+  providers: [
+    AuthService,
+    JwtAccessStrategy,
+    JwtRefreshStrategy,
+    CloudinaryService
+  ],
 })
 export class AuthModule { }
