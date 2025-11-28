c6e9d2729fe178cf1933be24231714e63f5d06da
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 12:10:28 +0545

    added global pipe options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, transform: true, forbidNonWhitelisted: false
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
