2263f7c9e79985aa7be50da6e563a8231502625d
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 12:05:50 +0545

    config:file upload configuration setup

---
+++ src/auth/config/file-upload.config.ts
@@ -0,0 +1,6 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs("fileupload", () => ({
+    allowedMimeTypes: ["image/jpeg", "image/png"],
+    maxSize: 2 * 1024 * 1024
+}))
