74feeb06095fe1c8b643486a280bced5dc73448a
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 12:11:23 +0545

    feat:added services to providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { FileValidationPipe } from './pipes/file-validation.pipe';
+import { CloudinaryService } from './cloudinary.service';
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, CloudinaryService, FileValidationPipe],
 })
 export class AuthModule { }
