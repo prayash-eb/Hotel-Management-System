43401bd0614e8672ac180f08f15f9ccdf611cfd1
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 16:45:28 +0545

    feat: jwt access and refresh guard and strategy implemented

---
+++ src/auth/guards/jwt.guard.ts
@@ -0,0 +1,20 @@
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtAccessGuard extends AuthGuard('jwt_access') {
+    handleRequest(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
+        return super.handleRequest(err, user, info, context, status);
+    }
+}
+
+
+@Injectable()
+export class JwtRefreshGuard extends AuthGuard('jwt_refresh') {
+    handleRequest(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
+        return super.handleRequest(err, user, info, context, status);
+    }
+}
+
+
+
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IAccessTokenPayload, IRefreshTokenPayload } from '../interfaces/token-payload.interface';
+import { UserService } from '../../user/user.service';
+import type { Request } from "express";
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt_access') {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly userService: UserService
+
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.getOrThrow("JWT_ACCESS_TOKEN_SECRET"),
+            passReqToCallback: true
+        });
+    }
+
+    async validate(req: Request, payload: IAccessTokenPayload) {
+        const user = await this.userService.findById(payload.id);
+        if (!user) {
+            throw new NotFoundException('User does not exist');
+        }
+
+        // validate against the database if the token still exists
+        // in case of user logging out before the token expires - token will be removed from the database
+        const accessToken = req.headers.authorization?.split("Bearer ")[1].trim()!;
+        const isSessionValid = await user.isAccessTokenValid(accessToken)
+        if (!isSessionValid) {
+            throw new UnauthorizedException({
+                message: "Invalid Access Token"
+            })
+        }
+        return user
+    }
+}
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt_refresh') {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly userService: UserService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.getOrThrow("JWT_REFRESH_TOKEN_SECRET"),
+            passReqToCallback: true
+        });
+    }
+
+    async validate(req: Request, payload: IRefreshTokenPayload) {
+        const user = await this.userService.findById(payload.id)
+        if (!user) {
+            throw new NotFoundException("User doesnot exist")
+        }
+
+        // validate against the database if the token still exists
+        // in case of user logging out before the token expires - token will be removed from the database
+        const refreshToken = req.headers.authorization?.split("Bearer ")[1].trim()!;
+
+        const isSessionValid = await user.isRefreshTokenValid(refreshToken)
+        if (!isSessionValid) {
+            throw new UnauthorizedException({
+                message: "Invalid Refresh Token"
+            })
+        }
+
+        return { user, refreshToken }
+    }
+}
