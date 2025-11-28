0d3958d8e60479f70b2938079e4c1b7788ca67b3
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 16:44:40 +0545

    role based auth setup using guard and decorator

---
+++ src/auth/decorators/role.decorator.ts
@@ -0,0 +1,9 @@
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from '../../user/schema/user.schema';
+
+export type RoleTypes = UserRole.ADMIN | UserRole.CUSTOMER | UserRole.HOTEL_OWNER
+
+export const ROLE_KEY = "roles";
+
+export const Roles = (...roles: RoleTypes[]) => SetMetadata(ROLE_KEY, roles)
+
