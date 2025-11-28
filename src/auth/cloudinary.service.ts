424ac617580d27af4077e1650bf1646cdcfe4aa7
prayash-eb <prayash.mishra@ebpearls.com>
2025-11-28 12:05:16 +0545

    feat:cloudinary upload image service implementation

---
+++ src/auth/cloudinary.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { v2 as cloudinary } from "cloudinary";
+import * as streamifier from 'streamifier'
+
+@Injectable()
+export class CloudinaryService {
+    constructor() {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET,
+        })
+    }
+
+    uploadImage(file: Express.Multer.File): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const upload = cloudinary.uploader.upload_stream(
+                { folder: 'profiles' },
+                (error, result) => {
+                    if (error) return reject(error);
+                    resolve(result);
+                },
+            );
+            streamifier.createReadStream(file.buffer).pipe(upload)
+        })
+    }
+}
